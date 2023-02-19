9f356c7ce6e361a0c8fa698b2d409ee3c8e53480
John Hooks <bitmachina@outlook.com>
2023-02-18 19:28:13 -0800

    build: replace yarn with pnpm

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,9 @@
 /* eslint-disable import/no-commonjs, functional/immutable-data, functional/no-expression-statement */
 module.exports = {
-  '**/*.ts': () => 'yarn test:types',
+  '**/*.ts': () => 'pnpm test:types',
   '!(website/)**/*.(js|mjs|ts|)': (filenames) =>
     `eslint --ext .js,.ts --fix ${filenames.join(' ')}`,
-  'website/**/*.(js|jsx|ts|tsx)': () => `yarn --cwd website lint --fix`,
+  'website/**/*.(js|jsx|ts|tsx)': () => `pnpm --cwd website lint --fix`,
   '**/*.(js|mjs|jsx|ts|tsx)': (filenames) =>
-    `yarn format ${filenames.join(' ')}`,
+    `pnpm format ${filenames.join(' ')}`,
 };
